5a6b47b0ad0aafdab69abaea887278eb758f9a03
rafaeelricco <rafaelricco10@gmail.com>
2024-10-13 16:50:33 -0300

    refactor: update health check configuration for ALB module

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -35,14 +35,12 @@ resource "aws_lb_target_group" "main" {
   vpc_id      = var.vpc_id
   target_type = "ip"
 
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    matcher             = "200"
-    timeout             = "3"
-    path                = var.health_check_path
-    unhealthy_threshold = "2"
+   health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
   }
 
   tags = {
