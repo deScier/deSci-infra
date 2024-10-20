4969d859c6da18a2b5a7c1a07892f07c1e9fbb96
rafaeelricco <rafaelricco10@gmail.com>
2024-10-20 16:12:54 -0300

    fix: updated aws_security_group ports to use the same of app port

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -97,11 +97,11 @@ resource "aws_security_group" "ecs_tasks" {
   description = "Security group for ECS tasks"
   vpc_id      = var.vpc_id
 
- ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  ingress {
+    from_port       = var.container_port
+    to_port         = var.container_port
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
   }
 
   ingress {
